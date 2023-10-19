57323c6b6cbe333e4948cc6b2b4efba2c641b216
PedroHAMota <henrypedrinho11@gmail.com>
2023-10-19 13:51:11 -0300

    cadastro1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'politica',
     loadChildren: () => import('./politica/politica.module').then( m => m.PoliticaPageModule)
+  },  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
   }
+
 
 ];
 
