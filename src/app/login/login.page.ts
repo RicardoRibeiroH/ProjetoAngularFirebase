007f543a1cffb46ac498078f4083370f0c4b6560
Ricardo Ribeiro <ricardoribeiro062106@gmail.com>
2023-10-26 13:56:29 +0000

    commit to EPA day 3

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,9 +8,16 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email: string = ''; // Adicionei um valor padrão aqui
+  senha: string = ''; // Adicionei um valor padrão aqui
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  clicarBotao(): void {
+    console.log('Botão clicado!');
+    // Adicione a lógica desejada aqui
+  }
 }
