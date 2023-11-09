256130e48d38f8ddad1edd427f12335ad6a1cf38
PedroHAMota <henrypedrinho11@gmail.com>
2023-11-09 16:17:46 -0300

    commit nova tela tcc

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {  ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
@@ -8,9 +9,40 @@ import { Routes, RouterModule } from '@angular/router';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public element: ElementRef, 
+    public renderer: Renderer2
+  ) { }
 
   ngOnInit() {
+  }
+
+  onRegisterClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.add('active');
+    }
+  }
+
+  onLoginClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.remove('active');
+    }
+  }
+
+  onBtnPopupClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.add('active-popup');
+    }
+  }
+
+  onIconCloseClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.remove('active-popup');
+    }
   }
 
 }
