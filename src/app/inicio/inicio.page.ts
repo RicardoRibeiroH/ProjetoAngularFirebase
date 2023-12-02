ed3eaebabfa71e98a89ec299c7108bd6d74fc7d3
PedroHAMota <henrypedrinho11@gmail.com>
2023-12-02 19:39:09 -0300

    commit tela tcc projeto Final

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -8,7 +9,101 @@ import { Routes, RouterModule } from '@angular/router';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
+
+  isMobileDevice(): boolean {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+
+  user = {
+    nome: null,
+    email: null,
+    conectado: false,
+    token: null
+  }
+
+  onRegisterClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.add('active');
+    }
+  }
+
+  onLoginClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.remove('active');
+    }
+  }
+
+  onBtnPopupClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.add('active-popup');
+    }
+  }
+
+  onIconCloseClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.remove('active-popup');
+    }
+  }
+
+  getDados(form: any){
+    let usuario = form;
+
+    fetch('http://localhost/tcc2/cadastro_usuario/login/validacaoLogin.php',
+			{
+			  method: 'POST',
+			  headers: {
+			    'Content-Type': 'application/json',
+			  },
+			  body: JSON.stringify(usuario)
+			}
+		)
+    .then(response => response.json())
+    .then(response => {
+      this.user = response;
+      console.log(this.user);
+      // if(response === true){
+      //   this.router.navigate(['/home']);
+      // }else{
+      //   console.log('erro');
+      // }
+    })    
+    .catch(erro => {
+      console.log(erro);
+    })
+    .finally(()=>{
+      
+      console.log('processo finalizado');
+    })
+  }
+
+  adicionarFuncionarios(form: any){
+    let usuario = form;
+    // console.log(form);
+    fetch('http://localhost/tcc2/cadastro_usuario/cadastro.php',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(usuario)
+    })
+    .then(resp => resp.json())
+    .then(resp=> {
+     
+      console.log(resp);
+    })
+    .catch(erro => {
+      console.log(erro);
+    })
+    .finally(()=>{   
+      console.log('processo finalizado');
+    })
+  }
 
   ngOnInit() {
   }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,15 +7,51 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  isMobileDevice(): boolean {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+
+  user = {
+    nome: null,
+    email: null,
+    conectado: false,
+    token: null
+  }
+
+  onRegisterClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.add('active');
+    }
+  }
+
+  onLoginClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.remove('active');
+    }
+  }
+
+  onBtnPopupClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.add('active-popup');
+    }
+  }
+
+  onIconCloseClick() {
+    const wrapper = document.querySelector('.wrapper');
+    if (wrapper) {
+      wrapper.classList.remove('active-popup');
+    }
   }
 
   getDados(form: any){
     let usuario = form;
 
-    fetch('http://localhost/tcc2/login/validacaoLogin.php',
+    fetch('http://localhost/tcc2/cadastro_usuario/login/validacaoLogin.php',
 			{
 			  method: 'POST',
 			  headers: {
@@ -26,12 +62,13 @@ export class LoginPage implements OnInit {
 		)
     .then(response => response.json())
     .then(response => {
-      console.log(response);
-      if(response === true){
-        this.router.navigate(['/inicio']);
-      }else{
-        console.log('erro');
-      }
+      this.user = response;
+      console.log(this.user);
+      // if(response === true){
+      //   this.router.navigate(['/home']);
+      // }else{
+      //   console.log('erro');
+      // }
     })    
     .catch(erro => {
       console.log(erro);
@@ -40,6 +77,33 @@ export class LoginPage implements OnInit {
       
       console.log('processo finalizado');
     })
+  }
+
+  adicionarFuncionarios(form: any){
+    let usuario = form;
+    // console.log(form);
+    fetch('http://localhost/tcc2/cadastro_usuario/cadastro.php',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(usuario)
+    })
+    .then(resp => resp.json())
+    .then(resp=> {
+     
+      console.log(resp);
+    })
+    .catch(erro => {
+      console.log(erro);
+    })
+    .finally(()=>{   
+      console.log('processo finalizado');
+    })
+  }
+
+  ngOnInit() {
   }
  
   
